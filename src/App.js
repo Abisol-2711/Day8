e962d43b4315d3bfd6b69cd25ba5f054dd88f873
Sol <solabigail00@hotmail.com>
2022-08-20 20:35:44 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import Index from "./pages/index";
+import Create from "./pages/create";
+import View from "./pages/view";
 import "./App.css";
-
-import EmojiPickerInput from "./components/emojiPicker/emojiPickerInput";
+import { Routes, Route } from "react-router-dom";
+import Store from "./store/store";
 
 function App() {
   return (
-    <div className="App">
-      <EmojiPickerInput />
-    </div>
+    <Store>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="create" element={<Create />} />
+          <Route path="view/:bookId" element={<View />} />
+        </Routes>
+      </div>
+    </Store>
   );
 }
 
